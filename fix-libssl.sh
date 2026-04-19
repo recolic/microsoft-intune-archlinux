1f61b16002d8ce6ae08e65931dc96e3c74da92b4
Recolic <git@me.recolic.net>
2026-04-18 23:44:07 -0700

    .remove.fix-libssl.sh

--- fix-libssl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This is a one-click script to fix the known BadCertificate problem. (Details in README.md)
-set -e
-
-echo "This script is not necessary for latest intune-portal anymore."
